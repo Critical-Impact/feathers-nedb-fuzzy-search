ad4e9760fd9bc5482fdd7c28d9d7df5bc86182b9
Blair Williams <blair.d.williams@gmail.com>
2019-08-26 05:09:26 +0100

    typing

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+// Type definitions for feathers-nedb-fuzzy-search 1.3.1
+// Definitions by: Blair Williams <https://nexvia.com.au>
+
+export = search;
+
+declare function search(options: FeathersNedbFuzzySearch.CheckPermissionsOptions): any;
+
+declare namespace FeathersNedbFuzzySearch {
+  export interface CheckPermissionsOptions
+  {
+    //Specify which fields to search.
+    fields?: string[];
+    //If true and fields is undefined, will search deep in objects. nedb search mode only
+    deep?: boolean;
+    //If true, diacritics will be ignored. 5x slower. nedb search mode only
+    fuzzyDiacritics: boolean;
+    //Specify which fields to exclude from search. nedb regex mode only
+    excludeFields: string[];
+  }
+}
